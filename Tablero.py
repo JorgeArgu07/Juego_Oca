5f4889d1d0c7fbb2f16ec8c066c76574f39c960c
Jorge Argumaniz <41338875+JorgeArgu07@users.noreply.github.com>
2019-11-26 17:01:57 -0600

    init

---
+++ Tablero.py
@@ -0,0 +1,11 @@
+import pygame, sys
+
+class Tablero:
+    def __init__(self):
+        self.x
+        self.y
+        self.img
+        self.width
+        self.height
+    def dibujar(self):
+        pygame.init()
