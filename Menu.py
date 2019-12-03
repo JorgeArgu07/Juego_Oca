b5bec8ba1a9bdb539a18760e6746be08411b56cf
jesusalcalaluna <jesusalcalaluna@users.noreply.github.com>
2019-12-03 17:31:50 -0600

    imagen fondo

--- Menu.py
+++ Menu.py
@@ -1,3 +1,14 @@
-class Menu:
-    def __init__(self):
-        pass
+from random import shuffle
+import pygame,sys
+from pygame.locals import *
+color = pygame.Color(200,200,200)
+pygame.init()
+window = pygame.display.set_mode((1280,680))
+pygame.display.set_caption('Juego de la Oca')
+
+while True:
+    window.fill(color)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit()
+    pygame.display.update()
